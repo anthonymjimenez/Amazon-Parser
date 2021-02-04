72a3c7da36e089cf33d331b264230028042f4675
anthonymjimenez <anthonyjim96@gmail.com>
2021-02-04 00:51:33 -0500

    formatting

--- fetchTest.js
+++ fetchTest.js
@@ -1,7 +1,10 @@
 var DomParser = require("dom-parser");
 var parser = new DomParser();
 const fetch = require("node-fetch");
-const url=["https://www.amazon.com/Fire-TV-Stick-4K-with-Alexa-Voice-Remote/dp/B079QHML21/ref=gbps_img_s-5_cd34_0d56241e?smid=ATVPDKIKX0DER&pf_rd_p=fd51d8cf-b5df-4144-8086-80096db8cd34&pf_rd_s=slot-5&pf_rd_t=701&pf_rd_i=gb_main&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=4MRB3MB8YTD1TJ7ZRG23", 'https://www.amazon.com/Xbox-Gift-Card-Digital-Code/dp/B00F4CEHNK/ref=zg_bs_videogames_home_2?_encoding=UTF8&psc=1&refRID=1AXP2J3GBVG6CMM6VVRY']
+const url = [
+  "https://www.amazon.com/Fire-TV-Stick-4K-with-Alexa-Voice-Remote/dp/B079QHML21/ref=gbps_img_s-5_cd34_0d56241e?smid=ATVPDKIKX0DER&pf_rd_p=fd51d8cf-b5df-4144-8086-80096db8cd34&pf_rd_s=slot-5&pf_rd_t=701&pf_rd_i=gb_main&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=4MRB3MB8YTD1TJ7ZRG23",
+  "https://www.amazon.com/Xbox-Gift-Card-Digital-Code/dp/B00F4CEHNK/ref=zg_bs_videogames_home_2?_encoding=UTF8&psc=1&refRID=1AXP2J3GBVG6CMM6VVRY",
+];
 
 const parseFromUrl = (url) =>
   fetch(url)
@@ -27,26 +30,34 @@ const amazonProtocol = (dom) => {
     ? domByID("priceblock_ourprice").innerHTML
     : domByID("priceblock_dealprice").innerHTML;
   var title = domInner("title")[0];
-  const description = 
-    domByID("feature-bullets").getElementsByClassName('a-list-item')[0].innerHTML.concat(` ${domByID("feature-bullets").getElementsByClassName('a-list-item')[1].innerHTML}`).replace(/(\r\n|\n|\r)/gm, "");
-
+  const description = domByID("feature-bullets")
+    .getElementsByClassName("a-list-item")[0]
+    .innerHTML.concat(
+      ` ${
+        domByID("feature-bullets").getElementsByClassName("a-list-item")[1]
+          .innerHTML
+      }`
+    )
+    .replace(/(\r\n|\n|\r)/gm, "");
 
   // still WIP -----
-//  console.log("meta", domOuter("meta"));
-    console.log(
+  //  console.log("meta", domOuter("meta"));
+  console.log(
     "img(d-o-h)",
     domByID("landingImage").getAttribute("data-old-hires")
   ); // 'data-old-hires' is more reliable than 'src' but they all seem to sometimes fail
   console.log("img(src)", domByID("landingImage").getAttribute("src"));
-  console.log(   // fallback option
+  console.log(
+    // fallback option
     "img(d-a-d-i)",
-    domByID("landingImage").getAttribute("data-a-dynamic-image"))
+    domByID("landingImage").getAttribute("data-a-dynamic-image")
+  );
 
   //           -----
 
   return { price: price, title: title, image: "WIP", description: description };
 };
 
-url.map(url => parseFromUrl(url).then(console.log))
+url.map((url) => parseFromUrl(url).then(console.log));
 
-// next step is to check with dozens of URLs 
+// next step is to check with dozens of URLs
