06ee5d9e34d19dc0dd10a6c1f5ba7eac4e1648d2
anthonymjimenez <anthonyjim96@gmail.com>
2021-02-10 19:58:00 -0500

    added category and exported URLS

--- fetchTest.js
+++ fetchTest.js
@@ -1,7 +1,4 @@
-var DomParser = require("dom-parser");
-var parser = new DomParser();
-const fetch = require("node-fetch");
-const url = [
+const urls = [
   "https://www.amazon.com/Fire-TV-Stick-4K-with-Alexa-Voice-Remote/dp/B079QHML21/ref=gbps_img_s-5_cd34_0d56241e?smid=ATVPDKIKX0DER&pf_rd_p=fd51d8cf-b5df-4144-8086-80096db8cd34&pf_rd_s=slot-5&pf_rd_t=701&pf_rd_i=gb_main&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=4MRB3MB8YTD1TJ7ZRG23",
   "https://www.amazon.com/Xbox-Gift-Card-Digital-Code/dp/B00F4CEHNK/ref=zg_bs_videogames_home_2?_encoding=UTF8&psc=1&refRID=1AXP2J3GBVG6CMM6VVRY",
   "https://www.amazon.com/dp/B084HK3CJN/ref=thedrp_hm_stlk_dp?theDropLookIdDpx=cda487a5-ec69-4088-a122-eeb04d774170",
@@ -14,68 +11,5 @@ const url = [
   "https://www.amazon.com/dp/B07KPT76KH"
 ];
 
-const parseFromUrl = (url) =>
-  fetch(url)
-    .then((html) => html.text())
-    .then((html) => {
-      var dom = parser.parseFromString(html);
-      // hopefully we can create multi protocols
-      // let parser = functionThatTakesDomAndDeterminesParser(dom) 
-      return { ...amazonParser(dom), url: url};
-    });
+exports.urls = urls
 
-const grabAndMapByTag = (dom, html) => (tag) =>
-  dom.getElementsByTagName(tag).map((m) => m[html]);
-
-const grabByID = (dom) => (id) => dom.getElementById(id);
-
-const amazonParser = (dom) => {
-  var [domOuter, domInner] = [
-    grabAndMapByTag(dom, "outerHTML"),
-    grabAndMapByTag(dom, "innerHTML"),
-  ];
-  var domByID = grabByID(dom);
-
-  var price = domByID("priceblock_ourprice")
-    ? domByID("priceblock_ourprice").innerHTML
-    : domByID("priceblock_dealprice")
-    ? domByID("priceblock_dealprice").innerHTML
-    : dom.getElementsByClassName("a-color-price") // for books, could replace with ""
-    ? dom.getElementsByClassName("a-color-price")[0].innerHTML
-    : "";
-  var title = domInner("title")[0];
-  const description = domByID("feature-bullets")
-    ? domByID("feature-bullets")
-        .getElementsByClassName("a-list-item")[1]
-        .innerHTML.concat(
-          ` ${
-            domByID("feature-bullets").getElementsByClassName("a-list-item")[2]
-              .innerHTML
-          }`
-        )
-        .replace(/(\r\n|\n|\r)/gm, "")
-    : "";
-  const image = domByID("landingImage")
-    ? domByID("landingImage").getAttribute("data-old-hires")
-    : "https://i1.wp.com/fremontgurdwara.org/wp-content/uploads/2020/06/no-image-icon-2.png";
-    
-  return { price: price, title: title, image: image, description: description };
-
-  // still WIP -----
-  // console.log("meta", domOuter("meta"));
-  // console.log(
-  //   "img(d-o-h)",
-  //   domByID("landingImage").getAttribute("data-old-hires")
-  // ); // 'data-old-hires' is more reliable than 'src' but they all seem to sometimes fail
-  // console.log(
-  //   // fallback option
-  //   "img(d-a-d-i)",
-  //   domByID("landingImage").getAttribute("data-a-dynamic-image")
-  // );
-
-  //           -----
-};
-
-url.map((url) => parseFromUrl(url).then(console.log));
-
-// next step is to check with dozens of URLs
